ebad62b719b65f696b2866e9fa50a4fa75d6a7ac
kitoxis <dauksa.laurynas@gmail.com>
2024-06-24 11:07:32 +0300

    test

---
+++ src/main/java/net/starfal/klocks/kLocks.java
@@ -0,0 +1,17 @@
+package net.starfal.klocks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class kLocks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
